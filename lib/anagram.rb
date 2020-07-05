1a2fab07456bcf4bc3fd5ede216e07b15f9f5396
foxten <tenaysiafox@gmail.com>
2020-07-05 12:10:30 -0500

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,22 @@
-# Your code goes here!
+class Anagram
+    attr_accessor :word
+    
+    def initialize(word)
+        @word = word
+    end
+
+    def match(possible_matches)
+        actual_matches = []
+        separated = possible_matches.map{|entry| entry.split(//)}
+        separated.each do |match|
+          if match.size == word.size && match.sort == word.split(//).sort
+            actual_matches << match.join("")
+          end
+        end
+        actual_matches
+    end
+end
+
+on = Anagram.new("on")
+possible_matches = ["hello", "goodbye", "yes", "no"]
+
